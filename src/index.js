c493b7de9322d7a0f556d1c8cdc0ab7c5c2a180f
Yeimer Gabriel Molina Ramirez <yeimer.molina@gmail.com>
2017-11-03 21:59:22 -0400

    Update main component

--- src/index.js
+++ src/index.js
@@ -1,13 +1,38 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-import SearchBar from './components/search_bar'
-
+import YTSearch from 'youtube-api-search';
+import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
 const API_KEY = 'AIzaSyCM2FuhJ3tNU3-aKi9OuasMUXK1Q_1ppvI';
 
-const App =  () => {
-  return <div>
-    <SearchBar/>
-  </div>
+YTSearch({key: API_KEY, term: 'linkin park'}, function(data){
+  console.log(data)
+});
+
+class App extends Component {
+  constructor(props){
+      super(props);
+      
+      this.state = { videos: [] };
+      
+      YTSearch({key: API_KEY, term: 'linkin park'}, videos => {
+        this.setState({ videos });
+      });
+      
+      
+  }
+  
+  render(){
+    return (
+      <div>
+        <SearchBar/>
+        <VideoDetail video={this.state.videos[0]}/>
+        <VideoList videos={this.state.videos} />
+      </div>
+    );
+  }
+  
 }
 
 ReactDOM.render(<App/>, document.querySelector('.container') );
