3bef0b6fbac1ef0c5a9355472ee94bb9a664d3bd
Yeimer Gabriel Molina Ramirez <yeimer.molina@gmail.com>
2017-11-04 20:28:21 -0400

    Display selected video when clicked

--- src/components/video_list.js
+++ src/components/video_list.js
@@ -2,9 +2,16 @@ import React from 'react';
 import VideoListItem from './video_list_item';
 
 const VideoList = (props) => {
+  console.log(props)
   const videoItems = props.videos.map((video) => {
-    return <VideoListItem key={video.etag} video={video}  />
+    return <VideoListItem 
+      key={video.etag} 
+      video={video}
+      onVideoSelect={props.onVideoSelect}  
+    />
   });
+  
+
   
   return (
     <ul className="col-md-4 list-group">
--- src/index.js
+++ src/index.js
@@ -14,10 +14,18 @@ class App extends Component {
   constructor(props){
       super(props);
       
-      this.state = { videos: [] };
+      this.state = { 
+          videos: [],
+          selectedVideo: null  
+      };
       
       YTSearch({key: API_KEY, term: 'linkin park'}, videos => {
-        this.setState({ videos });
+        this.setState(
+          { 
+            videos,
+            selectedVideo: videos[0]
+          }
+        );
       });
       
       
@@ -27,8 +35,11 @@ class App extends Component {
     return (
       <div>
         <SearchBar/>
-        <VideoDetail video={this.state.videos[0]}/>
-        <VideoList videos={this.state.videos} />
+        <VideoDetail video={this.state.selectedVideo}/>
+        <VideoList 
+          videos={this.state.videos} 
+          onVideoSelect={selectedVideo => this.setState({selectedVideo})}
+         />
       </div>
     );
   }
